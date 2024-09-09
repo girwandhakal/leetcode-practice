e0e293f786b085adaf2d76b205ea3b8d3035ac44
girwandhakal <gdhakal@crimson.ua.edu>
2024-09-08 23:30:50 -0500

    added answer

---
+++ 433 - Minimum Genetic Mutation.cpp
@@ -0,0 +1,61 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <queue>
+
+class Solution {
+public:
+    // check if a mutation is possible
+    bool canMutate(string startGene, string endGene)
+    {
+        int count = 0;
+        for(int i = 0; i < startGene.length(); i++)
+        {
+            if(startGene[i] != endGene[i])
+            {
+                count++;
+            }
+        }
+        if(count == 1)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    unordered_map<string,bool> set;
+
+    int minMutation(string startGene, string endGene, vector<string>& bank) {
+        queue<string> q;
+        q.push(startGene);
+        set[startGene] = true;
+        int output = 0; // number of mutations
+        while(!q.empty())
+        {
+            int size = q.size();
+            // for-loop is used to know if we visited every node in a level
+            for(int i = 0; i < size; i++)
+            {
+                string current = q.front();
+                q.pop();
+                if(current == endGene)
+                {
+                    return output;
+                }
+                //visit every adjacent node
+                for(string item:bank)
+                {
+                    if(canMutate(current, item) && set.find(item) == set.end())
+                    {
+                        set[item] = true;
+                        q.push(item);
+                    }
+                }
+
+            }
+            output++;
+        }
+        return -1;
+        
+    }
+};
